ceb7488c31c34b249589350335dea432648aa090
Mattias Ernlund <mattias@ernlund.net>
2025-03-13 18:05:59 +0100

    Change to navigate with HashRouter

--- musicbattle/src/main.tsx
+++ musicbattle/src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Kolla om vi kör på GitHub Pages
-const isGitHubPages = import.meta.env.MODE === "production";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={isGitHubPages ? "/quiz_answer_app" : "/"}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
