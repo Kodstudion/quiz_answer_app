a894853c137c2b75636ea05199eaa72101af9c48
Mattias Ernlund <mattias@ernlund.net>
2025-03-14 11:22:29 +0100

    Update credentials

--- musicbattle/src/firebaseConfig.ts
+++ musicbattle/src/firebaseConfig.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCaavJkUIje3zkaWvYxMePc14mibJ6Lebo",
-    authDomain: "musikkampen.firebaseapp.com",
-    databaseURL: "https://musikkampen-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "musikkampen",
-    storageBucket: "musikkampen.firebasestorage.app",
-    messagingSenderId: "10870804330",
-    appId: "1:10870804330:web:5bbbf4e20cd6ec11b2df13"
-  };
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: "musikkampen",
+  storageBucket: "musikkampen.firebasestorage.app",
+  messagingSenderId: "10870804330",
+  appId: "1:10870804330:web:5bbbf4e20cd6ec11b2df13",
+};
 
 // 🔥 Initiera Firebase och databasen
 const app = initializeApp(firebaseConfig);
